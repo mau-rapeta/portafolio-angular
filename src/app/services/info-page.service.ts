3bb36a56beef979613ce54b5cef9655c047fbde7
Mau <mauriciorapeta147@gmail.com>
2019-04-19 22:52:06 -0500

    fin seccion 4, interface data and animate

---
+++ src/app/services/info-page.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { InfoPage } from '../interfaces/info-page.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoPageService {
+
+  info: InfoPage = {};
+  cargada = false;
+
+  constructor( private http: HttpClient ) { 
+
+    console.log("Servicio de la página listo!!");
+
+    // Leer el archivo JSON
+    this.http.get('assets/data/data-page.json')
+    .subscribe( (resp: InfoPage) => {
+
+      this.cargada = true;
+      this.info = resp;
+
+      console.log(resp);
+
+    });
+
+  }
+}
