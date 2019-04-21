f7389dbec5f3fd2370ec8ba4368e53723817efc1
Mau <mauriciorapeta147@gmail.com>
2019-04-20 19:44:11 -0500

    Fin seccion 6, firebase and loading

--- src/app/services/info-page.service.ts
+++ src/app/services/info-page.service.ts
@@ -10,10 +10,18 @@ export class InfoPageService {
   info: InfoPage = {};
   cargada = false;
 
+  equipo: any[] = [];
+
   constructor( private http: HttpClient ) { 
 
     console.log("Servicio de la página listo!!");
 
+    this.cargarInfo();
+    this.cargarEquipo();
+
+  }
+
+  private cargarInfo() {
     // Leer el archivo JSON
     this.http.get('assets/data/data-page.json')
     .subscribe( (resp: InfoPage) => {
@@ -21,9 +29,18 @@ export class InfoPageService {
       this.cargada = true;
       this.info = resp;
 
-      console.log(resp);
-
     });
-
   }
+
+  private cargarEquipo() {
+    // Leyendo equipo desde firebase
+    this.http.get('https://angular-html-e7fbc.firebaseio.com/equipo.json')
+    .subscribe( (resp: any[]) => {
+
+      this.equipo = resp;
+      
+    });
+   
+  }
+
 }
